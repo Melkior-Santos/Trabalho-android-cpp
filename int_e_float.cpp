7102f22d66da2b0ab16b7cd118c647833ab29b69
Melkior Gabriel Paim dos Santos <melkiordossantos@gmail.com>
2025-11-12 08:36:15 -0300

    Adicionado o arquivo int_e_float.cpp e atualizado o TODO.md

---
+++ int_e_float.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  //Int representa um número inteiro, abaixo seguem alguns jeitos de declarar ou atribuir um valor à uma variável
+  int a = 5;
+  int b = 2, c = -1;
+  int d;
+  d = a+b+c;
+
+  cout << "Ints" << endl;
+  cout << "a: " << a << endl;
+  cout << "b: " << b << endl;
+  cout << "c: " << c << endl;
+  cout << "d: " << d << endl;
+
+  cout << endl;
+
+  //Floats em geral representam números com vírgula
+  float e = 3.9;
+  float f = -4.2, g = 2.3;
+  float h;
+  h = e*f*g;
+
+  cout << "Floats" << endl;
+  cout << "e: " << e << endl;
+  cout << "f: " << f << endl;
+  cout << "g: " << g << endl;
+  cout << "h: " << h << endl;
+ 
+  int e_int = e;
+  cout << "e convertido para int: " << e_int << endl; //Note que e_int não possui parte decimal
+
+
+
+
+
+  
+}
